809e53995f786b25c3e88d6d58b8cb58bcb1592f
Egor Bochkarev <bochkarevei@gmail.com>
2020-03-16 23:31:19 +0300

    Add checking rather docker instaled or not and return correct statick files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const Server = require('./server');
 const SecurityService = require('./services/oauth2/security-service');
+const { execFile } = require('child_process');
 
-SecurityService.start().then(() => {
-    Server.startServer(3000);
-}).catch((error) => {
-    console.log('Error:', error);
+execFile('docker', ['ps'], (err, out) => {
+    if (err) {
+        console.error('Please instal and run docker deamon')
+    } else {
+        SecurityService.start().then(() => {
+            Server.startServer(3000);
+        }).catch((error) => {
+            console.log('Error:', error);
+        })
+    }
 })
