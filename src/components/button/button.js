2caa052089ad4438d51619fa02560b673ff34b09
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-04 23:16:07 +0300

    Remove disable logic from button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -5,28 +5,14 @@ import './button.scss';
 
 function Button({icon, text, size, type, className, adaptive, children, onClick, ...props}) {
   text = text || children;
-  const [buttonType, setButtonType] = useState(type);
-  useEffect(()=> {
-    setButtonType(type);
-  }, [type]);
   const classes = [
     'button',
     size ? `button_size_${size}` : '',
-    buttonType ? `button_type_${buttonType}` : '',
+    type ? `button_type_${type}` : '',
     className || ''
   ];
   return (
-    <button className={classes.join(' ')} disabled={type === 'disabled'} {...props} onClick={() => {
-      if (onClick) {
-        setButtonType('disabled');
-        Promise.resolve(onClick()).then(() => {
-          setButtonType(type);
-        }).catch((e) => {
-          setButtonType(type);
-          console.error('Error on ckick', e);
-        });
-      }
-    }}>
+    <button className={classes.join(' ')} disabled={type === 'disabled'} {...props} onClick={() => onClick && onClick()}>
       { icon && <Icon icon={icon}></Icon> }
       { text && <span className={'text text_size_xxs button__text' + (adaptive ? ' button__text_adaptive' : '')}>{text}</span> }
     </button>
