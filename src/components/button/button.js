f591542aa2745f08c4556643551cf4e4dece5e7a
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-04 23:29:30 +0300

    fix button styles

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,20 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Icon from '../icon';
 import './button.scss';
 
 
-function Button({icon, text, size, type, className, adaptive, children, onClick, ...props}) {
+function Button(
+    {
+      icon, text, size, type,
+      className, adaptive,
+      children, onClick, ...props
+    }) {
   text = text || children;
   const classes = [
     'button',
     size ? `button_size_${size}` : '',
     type ? `button_type_${type}` : '',
+    adaptive ? 'button_adaptive': '',
     className || ''
   ];
   return (
-    <button className={classes.join(' ')} disabled={type === 'disabled'} {...props} onClick={() => onClick && onClick()}>
+    <button
+      className={classes.join(' ')}
+      disabled={type === 'disabled'}
+      {...props}
+      onClick={() => onClick && onClick()}
+    >
       { icon && <Icon icon={icon}></Icon> }
-      { text && <span className={'text text_size_xxs button__text' + (adaptive ? ' button__text_adaptive' : '')}>{text}</span> }
+      { text && <span className="button__text">{text}</span> }
     </button>
   );
 }
