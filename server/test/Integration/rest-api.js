ec5e4b429a7c1071e1eab44cdf56826067437d03
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-16 01:02:06 +0300

    Add test rest api

--- server/test/Integration/rest-api.js
+++ server/test/Integration/rest-api.js
@@ -1,19 +1,39 @@
 const axios = require('axios');
 
+const host = 'http://localhost:3000';
+
 class RestApi {
   static async checkHealth() {
-    return axios.get('http://localhost:3000/health');
+    return axios.get(`${host}/health`);
   }
   static async getConfig() {
-    return axios.get('http://localhost:3000/api/settings')
+    return axios.get(`${host}/api/settings`)
         .then(({data}) => data);
   }
   static async setConfig(settings) {
-    return axios.post('http://localhost:3000/api/settings', settings)
+    return axios.post(`${host}/api/settings`, settings)
         .then(({data}) => data);
   }
   static async deleteConfig() {
-    return axios.delete('http://localhost:3000/api/settings');
+    return axios.delete(`${host}/api/settings`);
+  }
+
+  static async getBuilds(offset, limit) {
+    const queryParams = limit ? `?offset=${offset}&limit=${limit}` : '';
+    return axios.get(`${host}/api/builds${queryParams}`)
+        .then(({data}) => data);
+  }
+  static async getBuild(id) {
+    return axios.get(`${host}/api/builds/${id}`)
+        .then(({data}) => data);
+  }
+  static async getBuildLog(id) {
+    return axios.get(`${host}/api/builds/${id}/logs`)
+        .then(({data}) => data);
+  }
+  static async runBuild(id) {
+    return axios.post(`${host}/api/builds/${id}`)
+        .then(({data}) => data);
   }
 }
 
