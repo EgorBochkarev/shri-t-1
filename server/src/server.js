5525f742c5835a0f3c0fd05ad1365eb59b5dca1d
Egor Bochkarev <bochkarevei@gmail.com>
2020-04-16 22:36:05 +0300

    Fix server path to index.html

--- server/src/server.js
+++ server/src/server.js
@@ -9,14 +9,16 @@ const startServer = function(port) {
   app.use(express.json());
   const staticPath = path.resolve(__dirname, '..', '..', 'dist');
   app.use(express.static(staticPath));
-  app.use('/history', express.static(staticPath));
-  app.use('/settings', express.static(staticPath));
   app.get('/health', (req, res) => {
     res.end();
   });
 
   Api.initSettingsApi(app, baseUrl);
   Api.initBuildsApi(app, baseUrl);
+
+  app.get('/*', (req, res) => {
+    res.sendFile(staticPath + '/index.html');
+  });
 
   app.listen(port);
 };
